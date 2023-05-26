64cfb35940b4756d674a07c1a4841d4941cbe724
jaythewebdev <jaythewebdev@gmail.com>
2023-05-26 14:50:48 +0530

    User API Module

---
+++ UserAPI/UserAPI/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MinLength(2,ErrorMessage ="Username should be minimum of 2 characters")]
+        public string Username { get; set; }
+
+        public byte[]? Password { get; set; }
+
+        public byte[]? HashKey { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Name should be minimum of 2 characters")]
+        public string Name { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        [Required]
+        [Range(18,100,ErrorMessage ="Enter a valid age between 18-100")]
+        public int Age { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ UserAPI/UserAPI/Services/UserRepo.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+using UserAPI.Interfaces;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public class UserRepo : IUser<int,User>
+    {
+        private readonly JWTUserContext _context;
+
+        public UserRepo(JWTUserContext context)
+        {
+            _context = context;
+        }
+
+        public User Add(User item)
+        {
+            try
+            {
+                _context.Users.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(item);
+            }
+            return null;
+        }
+
+        public User Delete(int id)
+        {
+            try
+            {
+                var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+                return user;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return null;
+        }
+
+        public User Get(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+            return user;
+        }
+
+        public ICollection<User> GetAll()
+        {
+            var user= _context.Users.ToList();
+            if (user.Count > 0)
+                return user; 
+            return null;
+        }
+
+        public User Update(User item)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == item.Username);
+            if (user != null)
+            {
+                user.Password=item.Password;
+                user.Name=item.Name;
+                user.Email=item.Email;
+                user.PhoneNumber=item.PhoneNumber;
+                user.Age=item.Age;
+                _context.Users.Update(user);
+                _context.SaveChanges();
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
